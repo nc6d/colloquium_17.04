06a44cba8ce46d979bac74b46eaea800e91c7312
Bohdan <58623074+nc6d@users.noreply.github.com>
2020-04-21 10:54:59 +0300

    added displaying of 1st  element and his index in array

--- col(1-34)/30.py
+++ col(1-34)/30.py
@@ -4,9 +4,10 @@ import numpy as np
 
 arr = [np.random.randint(-10, 10) for x in range(10)]
 print('Default array:\n', arr)
-
+c = 0
 new_arr = []
 for i in range(len(arr)):
     if i != 0:
         new_arr.append(arr[i])
-print("First index element:", arr[0], '\nArray without 1st element:\n', new_arr, "\nNew array's average value is", sum(new_arr)/len(arr))
+    i = c
+print(f"First index element: {arr[0]}[{c}]", '\nArray without 1st element:\n', new_arr, "\nNew array's average value is", sum(new_arr)/len(arr))
